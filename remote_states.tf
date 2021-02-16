6e92146a86666f0788679515c70ebddd5bda23f2
David Redmin <david.redmin@trio.dhs.gov>
2021-02-16 15:23:29 -0500

    Define all necessary remote states

---
+++ remote_states.tf
@@ -0,0 +1,65 @@
+# ------------------------------------------------------------------------------
+# Retrieves state data from a Terraform backend. This allows use of the
+# root-level outputs of one or more Terraform configurations as input data
+# for this configuration.
+# ------------------------------------------------------------------------------
+
+data "terraform_remote_state" "terraform" {
+  backend = "s3"
+
+  config = {
+    encrypt        = true
+    bucket         = "cisa-cool-terraform-state"
+    dynamodb_table = "terraform-state-lock"
+    profile        = "cool-terraform-backend"
+    region         = "us-east-1"
+    key            = "cool-accounts/terraform.tfstate"
+  }
+
+  workspace = "production"
+}
+
+data "terraform_remote_state" "users" {
+  backend = "s3"
+
+  config = {
+    encrypt        = true
+    bucket         = "cisa-cool-terraform-state"
+    dynamodb_table = "terraform-state-lock"
+    profile        = "cool-terraform-backend"
+    region         = "us-east-1"
+    key            = "cool-accounts/users.tfstate"
+  }
+
+  workspace = "production"
+}
+
+data "terraform_remote_state" "userservices_production" {
+  backend = "s3"
+
+  config = {
+    encrypt        = true
+    bucket         = "cisa-cool-terraform-state"
+    dynamodb_table = "terraform-state-lock"
+    profile        = "cool-terraform-backend"
+    region         = "us-east-1"
+    key            = "cool-accounts-userservices/terraform.tfstate"
+  }
+
+  workspace = "production"
+}
+
+data "terraform_remote_state" "userservices_staging" {
+  backend = "s3"
+
+  config = {
+    encrypt        = true
+    bucket         = "cisa-cool-terraform-state"
+    dynamodb_table = "terraform-state-lock"
+    profile        = "cool-terraform-backend"
+    region         = "us-east-1"
+    key            = "cool-accounts-userservices/terraform.tfstate"
+  }
+
+  workspace = "staging"
+}
