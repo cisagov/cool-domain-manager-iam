314d2aa3a81f72a0f98d7df5b72153ceb022af43
David Redmin <david.redmin@trio.dhs.gov>
2021-02-16 15:30:05 -0500

    Put provisioner users in the appropriate group

---
+++ group_membership.tf
@@ -0,0 +1,12 @@
+# Put provisioner users in the appropriate group
+resource "aws_iam_user_group_membership" "provisioner" {
+  provider = aws.users
+
+  for_each = { for username, attributes in var.users : username => "" if contains(attributes["roles"], "provisioner") }
+
+  user = data.aws_iam_user.users[each.key].user_name
+
+  groups = [
+    aws_iam_group.provisioner_users.name
+  ]
+}
