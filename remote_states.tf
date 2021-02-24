be79239f89a6616fe215f1f2ccbadb1085d81843
David Redmin <david.redmin@trio.dhs.gov>
2021-02-24 11:24:48 -0500

    Add remote states for cool-sharedservices-networking

--- remote_states.tf
+++ remote_states.tf
@@ -4,6 +4,36 @@
 # for this configuration.
 # ------------------------------------------------------------------------------
 
+data "terraform_remote_state" "sharedservices_networking_production" {
+  backend = "s3"
+
+  config = {
+    encrypt        = true
+    bucket         = "cisa-cool-terraform-state"
+    dynamodb_table = "terraform-state-lock"
+    profile        = "cool-terraform-backend"
+    region         = "us-east-1"
+    key            = "cool-sharedservices-networking/terraform.tfstate"
+  }
+
+  workspace = "production"
+}
+
+data "terraform_remote_state" "sharedservices_networking_staging" {
+  backend = "s3"
+
+  config = {
+    encrypt        = true
+    bucket         = "cisa-cool-terraform-state"
+    dynamodb_table = "terraform-state-lock"
+    profile        = "cool-terraform-backend"
+    region         = "us-east-1"
+    key            = "cool-sharedservices-networking/terraform.tfstate"
+  }
+
+  workspace = "staging"
+}
+
 data "terraform_remote_state" "terraform" {
   backend = "s3"
 
