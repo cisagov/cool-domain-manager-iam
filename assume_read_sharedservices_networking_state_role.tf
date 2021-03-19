f8b070b247c0cdd54f8e1bbe70a1880cecc12a19
David Redmin <david.redmin@trio.dhs.gov>
2021-03-18 22:31:24 -0400

    Use updated output from cool-sharedservices-networking
    This output changed in cool-sharedservices-networking#44.

--- assume_read_sharedservices_networking_state_role.tf
+++ assume_read_sharedservices_networking_state_role.tf
@@ -10,5 +10,5 @@ resource "aws_iam_group_policy_attachment" "assume_read_sharedservices_networkin
   # remote states (production and staging) point to the same
   # "assume_read_terraform_state_role_policy".  Therefore, it doesn't
   # matter which remote state we use here.
-  policy_arn = data.terraform_remote_state.sharedservices_networking_staging.outputs.assume_read_terraform_state_role_policy.arn
+  policy_arn = data.terraform_remote_state.sharedservices_networking_staging.outputs.read_terraform_state.assume_policy.arn
 }
